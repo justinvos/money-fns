00ee83bdcff82b9fb0f4b07cc1617bff7e53d12f
Justin <justinvosnz@gmail.com>
2019-12-28 14:53:30 +1300

    Rename functions and add floatToAmount

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
+/**
+ * Converts a float to the nearest amount value.
+ * 
+ * @param {number} float 
+ * @returns {string}
+ */
+function floatToAmount (float) {
+  const cents = BigInt(Math.round(float * 100))
+  return fromCents(cents)
+}
+
+/**
+ * Converts amount value into the bigint number of cents.
+ * 
+ * @param {string} amount 
+ * @returns {bigint}
+ */
 function toCents (amount) {
   const [dollars, cents] = amount.split('.')
   return BigInt(dollars) * BigInt(100) + BigInt(cents)
 }
 
+/**
+ * Converts a bigint number of cents into an amount value. 
+ * 
+ * @param {bigint} cents 
+ * @returns {string}
+ */
 function fromCents (cents) {
   const dollars = cents / BigInt(100)
   const remainder = cents % BigInt(100)
   return `${dollars}.${remainder < 10 ? '0' : ''}${remainder}`
 }
 
-function addMoney (...amounts) {
-  return sumMoney(amounts)
-}
-
-function subMoney (minuend, subtrahend) {
-  return fromCents(toCents(minuend) - toCents(subtrahend))
-}
-
-function sumMoney (amounts) {  
+/**
+ * Adds all of the amounts terms into a sum amount.
+ * 
+ * @param {string[]} amounts 
+ * @returns {string}
+ */
+function addAmounts (...amounts) {
   let total = BigInt(0)
 
   for (const amount of amounts) {
@@ -27,16 +48,45 @@ function sumMoney (amounts) {
   return fromCents(total)
 }
 
-function divMoney (dividend, divisor) {
-  return (Number(toCents(dividend)) / divisor) / 100
+/**
+ * Subtracts the subtrahend from the minuend to calculate the difference. I.e. difference = minuend - subtrahend
+ * 
+ * @param {string} minuend 
+ * @param {string} subtrahend 
+ * @returns {string}
+ */
+function subAmount (minuend, subtrahend) {
+  return fromCents(toCents(minuend) - toCents(subtrahend))
 }
 
-function mulMoney (factor, amount) {
-  return fromCents(BigInt(Math.round(factor * Number(toCents(amount)))))
+/**
+ * Multiplies all the amount factors into the product amount.
+ * 
+ * @param {string[]} amounts 
+ * @returns {string}
+ */
+function mulAmounts (...amounts) {
+  let product = '1.00'
+
+  for (const amount of amounts) {
+    product = mulAmount(product, amount)
+  }
+
+  return product
 }
 
-exports.addMoney = addMoney
-exports.subMoney = subMoney
-exports.sumMoney = sumMoney
-exports.divMoney = divMoney
-exports.mulMoney = mulMoney
+/**
+ * Multiplies all the two factors amount into the product amount.
+ * 
+ * @param {string} leftAmount 
+ * @param {string} rightAmount
+ * @returns {string}
+ */
+function mulAmount (leftAmount, rightAmount) {
+  return fromCents(toCents(leftAmount) * toCents(rightAmount) / BigInt(100))
+}
+
+exports.floatToAmount = floatToAmount
+exports.addAmounts = addAmounts
+exports.subAmount = subAmount
+exports.mulAmounts = mulAmounts
--- src/tests/general.test.js
+++ src/tests/general.test.js
@@ -1,21 +1,17 @@
-const { addMoney, divMoney, mulMoney, subMoney, sumMoney } = require('../index')
+const { addAmounts, floatToAmount, mulAmounts, subAmount } = require('../index')
 
-test('addMoney adds each amount parameter', async () => {
-  expect(addMoney('2.00', '2.00')).toBe('4.00')
+test('addAmounts adds each amount parameter', () => {
+  expect(addAmounts('1.00', '2.00')).toBe('3.00')
 })
 
-test('subMoney subtracts the second amount from the first', async () => {
-  expect(subMoney('3.00', '2.00')).toBe('1.00')
+test('subAmount subtracts the second amount from the first', () => {
+  expect(subAmount('3.00', '2.00')).toBe('1.00')
 })
 
-test('sumMoney totals an array of amounts', async () => {
-  expect(sumMoney(['3.00', '2.00', '2.00', '1.00', '4.00'])).toBe('12.00')
+test('mulAmounts multiples an amount by a factor', () => {
+  expect(mulAmounts('5.00', '3.00')).toBe('15.00')
 })
 
-test('divMoney finds the ratio between two amounts', async () => {
-  expect(divMoney('4.00', '2.00')).toBe(2)
-})
-
-test('mulMoney multiples an amount by a factor', async () => {
-  expect(mulMoney(5, '3.00')).toBe('15.00')
+test('floatToAmount converts the float to the nearest amount value', () => {
+  expect(floatToAmount(4.507)).toBe('4.51')
 })
