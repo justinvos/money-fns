79b1e061726154caf2aefab265aa862b80b8a7be
Justin <justinvosnz@gmail.com>
2020-02-11 14:38:48 +1300

    Replace jest with custom test runner

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/bin/"
-  ]
-}
--- src/tests/general.test.js
+++ src/tests/general.test.js
@@ -1,17 +1,24 @@
-const { addAmounts, floatToAmount, mulAmounts, subAmount } = require('../../dist/index.umd')
+import colors from 'colors'
+import { addAmounts, floatToAmount, mulAmounts, subAmount } from '../index.js'
 
-test('addAmounts adds each amount parameter', () => {
-  expect(addAmounts('1.00', '2.00')).toBe('3.00')
-})
+function test (label, result, expected) {
+  console.log(`${label}:`)
+  console.log(`  Result=${result}`)
+  console.log(`  Expected=${expected}`)
 
-test('subAmount subtracts the second amount from the first', () => {
-  expect(subAmount('3.00', '2.00')).toBe('1.00')
-})
+  if (result === expected) {
+    console.log('  PASS'.green)
+  } else {
+    console.log('  FAIL'.red)
+  }
+  console.log()
+}
 
-test('mulAmounts multiples an amount by a factor', () => {
-  expect(mulAmounts('5.00', '3.00')).toBe('15.00')
-})
 
-test('floatToAmount converts the float to the nearest amount value', () => {
-  expect(floatToAmount(4.507)).toBe('4.51')
-})
+test('addAmounts adds each amount item with default options', addAmounts(['1.00', '2.00']), '3.00')
+
+test('subAmount subtracts the second amount from the first', subAmount('3.00', '2.00'), '1.00')
+
+test('mulAmounts multiples an amount by a factor', mulAmounts(['5.00', '3.00']), '15.00')
+
+test('floatToAmount converts the float to the nearest amount value', floatToAmount(4.507), '4.51')
