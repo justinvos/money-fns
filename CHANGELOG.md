cb65cf97542e2ff79e9ddeaf6d67c64e86673abf
Justin Vos <justinvosnz@gmail.com>
2023-08-07 22:27:48 +1200

    v2.0.3: Add JSDoc comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.0.3] - 2023-08-07
+
+### Added
+
+- JSDoc function documentation
+
 ## [2.0.2] - 2023-01-05
 
 ### Fixed
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 type DoubleDigit = `${Digit}${Digit}`;
 export type Amount = `${bigint}.${DoubleDigit}`;
 
+/**
+ * Sums (adds) all the Amounts in an array.
+ */
 export function sumAmounts(amounts: Amount[]): Amount {
   const integers = amounts.map(toInteger);
   const integerTotal = integers.reduce((accumulator, integer) => {
@@ -10,18 +13,32 @@ export function sumAmounts(amounts: Amount[]): Amount {
   return toAmount(integerTotal);
 }
 
+/**
+ * Adds all the Amount parameters.
+ */
 export function addAmounts(...amounts: Amount[]): Amount {
   return sumAmounts(amounts);
 }
 
+/**
+ * Subtracts an amount from another.
+ * @param minuend {Amount} the base amount
+ * @param subtrahend {Amount} the amount to subtract off
+ */
 export function subtractAmount(minuend: Amount, subtrahend: Amount): Amount {
   return toAmount(toInteger(minuend) - toInteger(subtrahend));
 }
 
+/**
+ * Scales (multiplies) an Amount by an integer factor.
+ */
 export function scaleAmount(amount: Amount, factor: bigint): Amount {
   return toAmount(toInteger(amount) * factor);
 }
 
+/**
+ * Multiplies two Amounts.
+ */
 export function multiplyAmount(
   multiplier: Amount,
   multiplicand: Amount
@@ -29,34 +46,55 @@ export function multiplyAmount(
   return toAmount((toInteger(multiplier) * toInteger(multiplicand)) / 100n);
 }
 
+/**
+ * Divides the Amount by the divisor.
+ */
 export function divideAmount(amount: Amount, divisor: bigint): Amount {
   return toAmount(toInteger(amount) / divisor);
 }
 
+/**
+ * Returns a reduced Amount by a percentage amount.
+ */
 export function discountAmount(amount: Amount, percentage: number): Amount {
   return toAmount(
     (toInteger(amount) * (10000n - BigInt(percentage * 100))) / 10000n
   );
 }
 
+/**
+ * Checks if the Amount is postive (more than 0).
+ */
 export function isAmountPositive(amount: Amount): boolean {
   return toInteger(amount) > 0n;
 }
 
+/**
+ * Checks if the Amount is negative (less than 0).
+ */
 export function isAmountNegative(amount: Amount): boolean {
   return toInteger(amount) < 0n;
 }
 
+/**
+ * Checks if the Amount is exactly zero.
+ */
 export function isAmountZero(amount: Amount): boolean {
   return amount === "0.00";
 }
 
+/**
+ * Compares two Amounts. Useful for sorting.
+ */
 export function compareAmount(a: Amount, b: Amount): number {
   return Math.sign(Number(toInteger(a) - toInteger(b)));
 }
 
 export type CurrencyCode = "USD";
 
+/**
+ * Formats the Amount to have commas at each thousand-place.
+ */
 export function formatAmount(amount: Amount, currency: CurrencyCode): string {
   return Number(amount).toLocaleString("en-US", {
     minimumFractionDigits: 2,
@@ -66,10 +104,16 @@ export function formatAmount(amount: Amount, currency: CurrencyCode): string {
 
 const AMOUNT_REGEX = /^\-?\d+\.\d\d$/;
 
+/**
+ * Checks if the string is a valid Amount.
+ */
 export function isValidAmount(value: string): boolean {
   return new RegExp(AMOUNT_REGEX).test(value);
 }
 
+/**
+ * Converts a number to the Amount datatype.
+ */
 export function floatToAmount(float: number): Amount {
   const full = float > 0 ? Math.floor(float) : Math.ceil(float);
   const sub = BigInt(Math.abs(Math.round((float % 1) * 100)));
